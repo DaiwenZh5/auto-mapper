d8f97dae55ae6c6f8fc9dd082463f0300c3b885c
daiwenzh5 <daiwenzh5@163.com>
2020-07-18 14:51:33 +0800

    feat(config): 添加 spring boot 自动配置

--- src/main/java/com/daiwenzh5/mapper/MapperAutoConfiguration.java
+++ src/main/java/com/daiwenzh5/mapper/MapperAutoConfiguration.java
@@ -1,8 +1,34 @@
 package com.daiwenzh5.mapper;
 
+import com.daiwenzh5.mapper.plugin.ResultSetHandlerInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author daiwenzh5
  * @date 2020-07-18 01:17
  */
+@Configuration
+@ConditionalOnBean(SqlSessionFactory.class)
+@AutoConfigureAfter(MybatisAutoConfiguration.class)
 public class MapperAutoConfiguration {
+
+    @Resource
+    private List<SqlSessionFactory> sqlSessionFactoryList;
+
+    @PostConstruct
+    public void addInterceptor() {
+        Interceptor interceptor = new ResultSetHandlerInterceptor();
+        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
+            sqlSessionFactory.getConfiguration().addInterceptor(interceptor);
+        }
+    }
 }
